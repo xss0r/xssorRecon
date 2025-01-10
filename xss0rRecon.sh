d0a9af4d49ecc434d201aa3171aa607603ed1dc0
xss0r <xss0r@proton.me>
2025-01-10 16:27:04 -0500

    Update xss0rRecon.sh

--- xss0rRecon.sh
+++ xss0rRecon.sh
@@ -190,6 +190,9 @@ install_tools() {
     sudo apt update --fix-missing
     sudo apt install pip
     sudo pip3 uninstall -y subprober
+    sudo pip uninstall subprober --break-system-packages
+    sudo apt install -y python3.12
+    sudo apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libnss3-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
     rm -r /usr/local/bin/subprober
     rm -r ~/.local/bin/subprober 
     rm -r /root/.local/bin/subprober
