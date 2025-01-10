b3a463042869c02a7e836bb31c1055d81b7a641c
xss0r <xss0r@proton.me>
2025-01-10 07:45:35 -0500

    Update xss0rRecon.sh

--- xss0rRecon.sh
+++ xss0rRecon.sh
@@ -194,6 +194,9 @@ install_tools() {
     rm -r ~/.local/bin/subprober 
     rm -r /root/.local/bin/subprober
     sudo pip install "subprober<2.0"
+    sudo apt install python3.12-venv
+    source .venv/bin/activate
+    python3 -m venv .venv
     sudo pip install colorama --break-system-packages
     pip install aiodns --break-system-packages
     pip install aiofiles --break-system-packages
