35ec57ad6d699bb1d05b0028a0ce6e03e4bb5d82
xss0r <xss0r@proton.me>
2025-01-29 12:25:06 -0500

    Update xss0rRecon.sh

--- xss0rRecon.sh
+++ xss0rRecon.sh
@@ -199,8 +199,7 @@ install_tools() {
     echo -e "${BOLD_WHITE}You selected: Install all tools${NC}"
 
     show_progress "Installing dependencies"
-    echo '$nrconf{restart} = "l";' | sudo tee /etc/needrestart/needrestart.conf && sudo apt update && sudo apt install needrestart -y && sudo apt upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" && sudo apt dist-upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" && sudo dpkg --configure -a && sudo apt -f install -y && sudo needrestart -q -r l
-    sudo apt update --fix-missing
+    sudo apt update && sudo apt install needrestart -y && sudo apt upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" && sudo apt dist-upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" && sudo dpkg --configure -a && sudo apt -f install -y && sudo needrestart -q -n    sudo apt update --fix-missing
     sudo apt install python3.12-venv
     python3 -m venv .venv
     source .venv/bin/activate 
