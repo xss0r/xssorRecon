300e1876436d4b52aff3f2825a0fab68cdc664dd
xss0r <xss0r@proton.me>
2025-01-29 09:36:51 -0500

    Update xss0rRecon.sh

--- xss0rRecon.sh
+++ xss0rRecon.sh
@@ -199,7 +199,7 @@ install_tools() {
     echo -e "${BOLD_WHITE}You selected: Install all tools${NC}"
 
     show_progress "Installing dependencies"
-    sudo apt update && sudo apt upgrade -y
+    sudo apt update && sudo apt install needrestart -y && sudo apt upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" && sudo apt dist-upgrade -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" && sudo dpkg --configure -a && sudo apt -f install -y && sudo needrestart -q -n
     sudo apt update --fix-missing
     sudo apt install python3.12-venv
     python3 -m venv .venv
